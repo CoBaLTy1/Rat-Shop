db524a32e43cca9c8567866aa3f33a1e3a28558f
CoBaLTy1 <ardakhalili4@gmail.com>
2024-08-29 13:48:31 +0200

    javascript.js

--- javascript.js
+++ javascript.js
@@ -12,14 +12,21 @@ function buying_shy_rat() {
             money -= 500;
     document.getElementById("money-display").textContent = "Money: " + money
     } else {
-        document.getElementById("money-display").textContent = "Money: " + money + " You dont have enough money" 
+        document.getElementById("money-display").textContent = "Money: " + money
+        document.getElementById("notenoughmoney").textContent = "You dont have enough money"
     }
 }
 
 
 function buying_sigma_rat() {
+    if (money >= 3500) {
     money -=3500;
     document.getElementById("money-display").textContent = "Money: " + money
+    } else {
+        document.getElementById("money-display").textContent = "Money: " + money + " You dont have enough money" 
+
+    }
+
 }
 
 function buying_liam() {
