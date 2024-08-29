2ae9bfb1d850f2c0dbd985a15b96446a5f365566
CoBaLTy1 <ardakhalili4@gmail.com>
2024-08-29 11:50:55 +0200

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+let money = 0;
+
+
+function addvalue() {
+    money += 10; 
+    document.getElementById("money-display").textContent = "Money: " + money; 
+}
+
+function buying_shy_rat() {
+    if (money >= 500) {
+            money -= 500;
+    document.getElementById("money-display").textContent = "Money: " + money
+    } else {
+        document.getElementById("money-display").textContent = "Money: " + money + " You dont have enough money" 
+    }
+}
+
+
+function buying_sigma_rat() {
+    money -=3500;
+    document.getElementById("money-display").textContent = "Money: " + money
+}
+
+function buying_liam() {
+    money -=5000;
+    document.getElementById("money-display").textContent = "Money: " + money
+}
